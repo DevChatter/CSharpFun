8ce9366b4495f3ba3dedcc828dff5d563b6a80e4
Brendan Enrick <brendan@enrick.com>
2018-03-08 12:26:16 -0500

    Adding 7.1 Samples

---
+++ src/CSharp7_1.Web/Pages/DefaultLiteralExpressions.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSharp7_1.Web.Pages
+{
+    public class DefaultLiteralExpressionsModel : PageModel
+    {
+        public string GetStringValue()
+        {
+            string oldWay = default(string);
+
+            string newWay = default;
+
+            return oldWay == newWay
+                ? "\"string oldWay = default(string)\" and \"string newWay = default;\" are the same"
+                : "This shouldn't happen...";
+        }
+    }
+}
---
+++ src/CSharp7_1.Web/Pages/InferredTupleElementNames.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSharp7_1.Web.Pages
+{
+    public class InferredTupleElementNamesModel : PageModel
+    {
+        public string OldWay()
+        {
+            int count = 5;
+            string label = "Colors used in the map";
+            var pair = (count: count, label: label);
+            return $"var pair = (count: count, label: label) is pair.count:{pair.count} and pair.label:{pair.label}";
+        }
+
+        public string NewWay()
+        {
+            int count = 5;
+            string label = "Colors used in the map";
+            var pair = (count, label); // element names are "count" and "label"
+            return $"var pair = (count, label) is pair.count:{pair.count} and pair.label:{pair.label}";
+        }
+    }
+}

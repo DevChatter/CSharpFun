62d7a0e5620dc6cab1c1e1a3bbbb62bebc5e61fc
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-03-28 12:16:30 -0400

    Adding ref and ref readonly structs

--- src/CSharp7_2.Web/Pages/ReferenceSemanticsValueTypes.cshtml.cs
+++ src/CSharp7_2.Web/Pages/ReferenceSemanticsValueTypes.cshtml.cs
@@ -5,6 +5,7 @@ namespace CSharp7_2.Web.Pages
 {
     public class ReferenceSemanticsValueTypesModel : PageModel
     {
+        private static BigStruct[] bigStructs = { new BigStruct {X = 1, Y = 2, Z = 3},  };
         public string GetInString()
         {
             int radius = 2;
@@ -19,5 +20,59 @@ namespace CSharp7_2.Web.Pages
         {
             return Math.PI * radius * radius;
         }
+
+        public string GetRefReturnString()
+        {
+            ref readonly BigStruct bigStruct = ref GetRefReturn();
+            //bigStruct.X = 7; // compiler error
+            string string1 = $" ({bigStruct.X} {bigStruct.Y} { bigStruct.Z}) ";
+            UseBigStruct(bigStruct); // Passes a copy
+            string string2 = $" ({bigStruct.X} {bigStruct.Y} { bigStruct.Z}) ";
+            return string1 + string2;
+        }
+
+        private static ref readonly BigStruct GetRefReturn()
+        {
+            return ref bigStructs[0];
+        }
+        private static void UseBigStruct(BigStruct thing)
+        {
+            thing.X += 5;
+        }
+    }
+
+    public struct BigStruct
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+    }
+
+    public readonly struct HeavilyPassedReadonlyStruct
+    {
+        public HeavilyPassedReadonlyStruct(Char symbol, int x, int y) 
+            => (Symbol, X, Y) = (symbol, x, y);
+
+        public Char Symbol { get; }
+        public int X { get; }
+        public int Y { get; }
+    }
+    public ref struct HeavilyPassedRefStruct
+    {
+        public HeavilyPassedRefStruct(Char symbol, int x, int y) 
+            => (Symbol, X, Y) = (symbol, x, y);
+
+        public Char Symbol { get; }
+        public int X { get; }
+        public int Y { get; }
+    }
+    public readonly ref struct HeavilyPassedReadonlyRefStruct
+    {
+        public HeavilyPassedReadonlyRefStruct(Char symbol, int x, int y) 
+            => (Symbol, X, Y) = (symbol, x, y);
+
+        public Char Symbol { get; }
+        public int X { get; }
+        public int Y { get; }
     }
 }

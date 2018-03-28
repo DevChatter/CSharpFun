f10f755c282d805fb368938a765731574d6f2834
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-03-28 12:31:30 -0400

    adding private protected

---
+++ src/CSharp7_2.Web/Pages/PrivateProtectedAccess.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSharp7_2.Web.Pages
+{
+    public class PrivateProtectedAccessModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+
+    // Assembly1.cs  
+    // Compile with: /target:library  
+    public class BaseClass
+    {
+        private protected int myValue = 0;
+    }
+
+    public class DerivedClass1 : BaseClass
+    {
+        void Access()
+        {
+            BaseClass baseObject = new BaseClass();
+
+            // Error CS1540, because myValue can only be accessed by
+            // classes derived from BaseClass.
+            // baseObject.myValue = 5;  
+
+            // OK, accessed through the current derived class instance
+            myValue = 5;
+        }
+    }
+}

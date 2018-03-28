2b92bf4e12b834469140ba7f65a5d57d863d1ea3
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-03-28 11:36:17 -0400

    Adding in example

--- src/CSharp7_2.Web/Pages/ReferenceSemanticsValueTypes.cshtml.cs
+++ src/CSharp7_2.Web/Pages/ReferenceSemanticsValueTypes.cshtml.cs
@@ -5,9 +5,19 @@ namespace CSharp7_2.Web.Pages
 {
     public class ReferenceSemanticsValueTypesModel : PageModel
     {
-        public void OnGet()
+        public string GetString()
         {
-            throw new NotImplementedException();
+            int radius = 2;
+            const int constRad = 3;
+            double fromVar = CalculateArea(in radius);
+            double fromConst = CalculateArea(constRad);
+            double fromLiteral = CalculateArea(7);
+            return $"{fromVar} {fromConst} {fromLiteral}";
+        }
+
+        private static double CalculateArea(in int radius)
+        {
+            return Math.PI * radius * radius;
         }
     }
 }

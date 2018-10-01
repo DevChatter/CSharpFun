02d3f47e4db0ebac799858ee2a819012684d4a14
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-09-30 21:38:05 -0400

    Adding 7.3

---
+++ src/CSharp7_3.Web/Pages/EnumConstraint.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSharp7_3.Web.Pages
+{
+    public class EnumConstraintModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+private string GetName<T>(T obj) where T : System.Enum
+{
+    return System.Enum.GetName(obj.GetType(), obj);
+}
+    }
+
+    public enum MyEnum
+    {
+        Zero,
+        One,
+        Two,
+        Three
+    }
+}
---
+++ src/CSharp7_3.Web/Pages/TupleEquality.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSharp7_3.Web.Pages
+{
+    public class TupleEqualityModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}

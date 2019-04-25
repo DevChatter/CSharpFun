a46e5aad786edfaf460c21d1a258295be0219285
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2019-04-24 21:20:02 -0400

    Adding Ranges Example

---
+++ src/CSharp8.Web/Pages/Ranges.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSharp8.Web.Pages
+{
+    public class RangesModel : PageModel
+    {
+        public int[] Numbers { get; set; } = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        public void OnGet()
+        {
+        }
+    }
+}
